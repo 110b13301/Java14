f5d6a327267a004a0bd55d87ed48d7eeca161bff
110b13301 <112999887+110b13301@users.noreply.github.com>
2022-12-20 10:55:26 +0800

    Delete Java02.java

--- Java02.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-import java.util.Scanner;
-public class Java02 {
-	public static void main(String args[]) {
-		Scanner sc=new Scanner(System.in);
-		String str=sc.next();
-		System.out.print(str.toUpperCase());
-	}
-}
